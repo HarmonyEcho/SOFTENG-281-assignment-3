9ccfa3a3b3907e866a0ebc739db3342fa1d6ffee
Kieran Joe <kjoe685@aucklanduni.ac.nz>
2024-05-28 22:58:11 +1200

    added overridden hashCode and equals methods to Country class

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -33,4 +33,31 @@ public class Country {
   public int getTax() {
     return this.tax;
   }
+
+  /** Taken from https://softeng281.digitaledu.ac.nz/topics/data-structures/. */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
+  }
+
+  /**
+   * Taken from https://softeng281.digitaledu.ac.nz/topics/data-structures/.
+   *
+   * @param obj an object to compare this Country to
+   * @return whether obj is equal to this Country
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Country other = (Country) obj;
+    if (name == null) {
+      if (other.name != null) return false;
+    } else if (!name.equals(other.name)) return false;
+    return true;
+  }
 }
