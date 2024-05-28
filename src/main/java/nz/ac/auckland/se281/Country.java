40e534c23358fdfe31ceb4ca81d535eef86a4471
Kieran Joe <kjoe685@aucklanduni.ac.nz>
2024-05-28 16:14:49 +1200

    Added getter methods to Country class

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -4,7 +4,7 @@ public class Country {
 
   private String name; // name of the country
   private String continent; // name of the continent containing the country
-  private int tax; // cost to travel into the country
+  private int tax; // tax to travel into the country
 
   /**
    * Constructor method
@@ -17,5 +17,20 @@ public class Country {
     this.name = name;
     this.continent = continent;
     this.tax = tax;
+  }
+
+  /** Getter method. Returns the name of the country as a string. */
+  public String getName() {
+    return this.name;
+  }
+
+  /** Getter method. Returns the name of the country's continent as a string. */
+  public String getContinent() {
+    return this.continent;
+  }
+
+  /** Getter method. Returns the name of the country's cross-border tax as a string. */
+  public int getTax() {
+    return this.tax;
   }
 }
